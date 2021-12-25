bef13679a8c47cfbfcf7f8aba7f154f5d93b3a70
duttrohan0302 <duttrohan0302@gmail.com>
2021-12-25 11:53:54 +0530

    changes

--- dec25th_calculator/script.js
+++ dec25th_calculator/script.js
@@ -25,8 +25,8 @@ C => display should get empty
 
 let displayxyz = document.getElementById("display")
 
-// let buttonsxyz = Array.from(document.getElementsByClassName("btn"))
-
+let buttonsxyz = (document.getElementsByClassName("btn"))
+console.log(buttonsxyz)
 // // btnxyz
 
 // function clickingxyz(btnxyz){
@@ -60,12 +60,21 @@ let displayxyz = document.getElementById("display")
 
 // Alternate Method
 
+function isOpxyz(opxyz){
+    if(opxyz==="+" || opxyz==='-' || opxyz==='/' || opxyz==='*')
+        return true
+    return false
+}
 function clickingxyz(clickedxyz){
     if(displayxyz.innerText===""){
         if(clickedxyz!='0'){
             displayxyz.innerText = displayxyz.innerText+clickedxyz
         }
     }else{
+        // Just to make sure that 2 operators don't come in consecutive positions
+        if(isOpxyz(clickedxyz) && isOpxyz(displayxyz.innerText[displayxyz.innerText.length-1])){
+            displayxyz.innerText = displayxyz.innerText.slice(0,-1)
+        }
         displayxyz.innerText = displayxyz.innerText+clickedxyz
     }
 }
