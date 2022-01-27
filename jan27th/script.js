8b6ba18f65dc7dd3c3af21b8eb0231eeb31c027e
duttrohan0302 <duttrohan0302@gmail.com>
2022-01-27 21:03:44 +0530

    removed playGame var

--- jan27th/script.js
+++ jan27th/script.js
@@ -1,84 +1,78 @@
-const submit = document.querySelector("#subt")
-const userInput = document.querySelector("#guessField")
-const guessSlot = document.querySelector(".guesses")
-const remaining = document.querySelector(".lastResult")
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
 
-const startOver = document.querySelector(".resultParas")
-const lowOrHi = document.querySelector(".lowOrHi")
+const startOver = document.querySelector(".resultParas");
+const lowOrHi = document.querySelector(".lowOrHi");
 
-let previousGuesses=[]
+let previousGuesses = [];
 let numGuesses = 0;
 
-let randomNumber = parseInt(Math.random()*100+1)
-let maxGuesses = 10
+let randomNumber = parseInt(Math.random() * 100 + 1);
+let maxGuesses = 10;
 
-let playGame = true;
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
 
-if(playGame){
-    submit.addEventListener("click",function(e){
-        e.preventDefault();
-    
-        const guess = parseInt(userInput.value)
-        validateGuess(guess)
-    })
+  const guess = parseInt(userInput.value);
+  validateGuess(guess);
+});
+
+function validateGuess(guess) {
+  previousGuesses.push(guess);
+
+  if (previousGuesses.length === maxGuesses) {
+    // Game over
+    displayGuesses(guess);
+    displayMsg(`Game Over! Number was ${randomNumber}`);
+    endGame();
+  } else {
+    displayGuesses(guess);
+    checkGuess(guess);
+  }
 }
 
+function displayGuesses(guess) {
+  guessSlot.innerHTML += `${guess} `;
+  // guessSlot.innerHTML= guessSlot.innerHTML + `${guess} `
+  userInput.value = "";
+  numGuesses++;
 
-function validateGuess(guess){
-    previousGuesses.push(guess)
+  let remainingGuesses = maxGuesses - numGuesses;
 
-    if(previousGuesses.length===maxGuesses){
-        // Game over
-        displayGuesses(guess)
-        displayMsg(`Game Over! Number was ${randomNumber}`)
-        endGame()
-    }else{
-        displayGuesses(guess)
-        checkGuess(guess)
-    }
+  if (remainingGuesses < 0) {
+    remainingGuesses = 0;
+  }
+  remaining.innerHTML = remainingGuesses;
+}
+function displayMsg(message) {
+  lowOrHi.innerHTML = `<h1>${message}</h1>`;
+}
+function checkGuess(guess) {
+  if (guess === randomNumber) {
+    displayMsg("You guessed correctly!");
+    endGame();
+  } else if (guess < randomNumber) {
+    displayMsg("Too low! Try a higher number");
+  } else {
+    displayMsg("Too High! Try a smaller number");
+  }
 }
 
-function displayGuesses(guess){
-    guessSlot.innerHTML+=`${guess} `
-    // guessSlot.innerHTML= guessSlot.innerHTML + `${guess} `
-    userInput.value=''
-    numGuesses++;
+const pEl = document.createElement("p");
+function endGame() {
+  userInput.value = "";
+  pEl.classList.add("button");
+  pEl.style.cursor = "pointer";
+  pEl.innerHTML = `<h1 onclick="newGame()" id="newGame">Start New Game</h1>`;
 
-    let remainingGuesses = maxGuesses - numGuesses
+  startOver.append(pEl);
 
-    if(remainingGuesses<0){
-        remainingGuesses = 0
-    }
-    remaining.innerHTML = remainingGuesses
-}
-function displayMsg(message){
-    lowOrHi.innerHTML = `<h1>${message}</h1>`
-}
-function checkGuess(guess){
-    if(guess === randomNumber){
-        displayMsg("You guessed correctly!")
-        endGame()
-    }else if(guess<randomNumber){
-        displayMsg("Too low! Try a higher number")
-    }else{
-        displayMsg("Too High! Try a smaller number")
-    }
+  userInput.disabled = true;
+  submit.disabled = true;
 }
 
-const pEl = document.createElement("p")
-function endGame(){
-    userInput.value=''
-    pEl.classList.add("button")
-    pEl.style.cursor = "pointer"
-    pEl.innerHTML = `<h1 onclick="newGame()" id="newGame">Start New Game</h1>`
-
-    startOver.append(pEl)
-
-    playGame=false
-    userInput.disabled = true
-    submit.disabled = true;
-}
-
-function newGame(){
-    location.reload()
+function newGame() {
+  location.reload();
 }
