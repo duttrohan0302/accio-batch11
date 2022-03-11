e9ec7d3ef5b3f812abf6e441ee415ae972c02973
duttrohan0302 <duttrohan0302@gmail.com>
2022-03-11 20:08:18 +0530

    redirects in netlify router

--- mar10th/bootstrap/src/App.js
+++ mar10th/bootstrap/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import App2 from "./App2";
 import App3 from "./App3";
@@ -8,11 +8,19 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div> Go to either /form2 or /form3</div>}></Route>
-        <Route path="/form2" element={<App2 />}>
-        </Route>
-        <Route exact path="/form3" element={<App3 />}>
-        </Route>
+        <Route
+          path="/"
+          element={<div> Go to either
+            <br />
+            <Link to="/form2">/form2</Link>
+            <br />
+            or
+            <br />
+            <Link to="/form3">/form3</Link>
+            </div>}
+        ></Route>
+        <Route path="/form2" element={<App2 />}></Route>
+        <Route path="/form3" element={<App3 />}></Route>
       </Routes>
     </BrowserRouter>
   );
